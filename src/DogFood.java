093320059579c3e8e5b07dd476c67f02f3aaaceb
mcree <mcree@10.20.19.118>
2019-10-22 12:07:54 -0500

    Done with dogfood

--- src/DogFood.java
+++ src/DogFood.java
@@ -32,7 +32,7 @@ public class DogFood
    public double getAmount()
    {
 	   double tcups = numcups()*7;
-	   amount = tcups/10+1;
+	   amount = tcups/60;
    	return amount;
    }
    
@@ -45,26 +45,33 @@ public class DogFood
 		   int weight = Integer.parseInt(pounds[i]);
 		   if(weight>=2 && weight<=4)
 			   cups+=0.5;
-		   if(weight>=5 && weight<=7)
-			   cups+=1;
-		   if(weight>=8 && weight<=9)
-			   cups+=1.5;
-		   if(weight>=10 && weight<=19)
-			   cups+=2;
-		   if(weight>=20 && weight<=39)
-			   cups+=3.5;
-		   if(weight>=40 && weight<=59)
-			   cups+=4.5;
-		   if(weight>=60 && weight<=79)
-			   cups+=6;
-		   if(weight>=80)
-			   cups+=7.5;
+		   else
+			   if(weight>=5 && weight<=7)
+				   cups+=1;
+			   else
+				   if(weight>=8 && weight<=9)
+					   cups+=1.5;
+				   else
+					   if(weight>=10 && weight<=19)
+						   cups+=2;
+					   else
+						   if(weight>=20 && weight<=39)
+							   cups+=3.5;
+						   else
+							   if(weight>=40 && weight<=59)
+								   cups+=4.5;
+							   else
+								   if(weight>=60 && weight<=79)
+									   cups+=6;
+								   else
+									   if(weight>=80)
+										   cups+=7.5;
 	   }
-	   return cups;
+	   return (cups);
    }
 
 	public String toString()
 	{
-		return this.amount + " of the 10 pound bags are needed.";
+		return Math.ceil(getAmount()) + " of the 10 pound bags are needed.";
 	}
 }
--- src/DogFoodRunner.java
+++ src/DogFoodRunner.java
@@ -4,15 +4,21 @@
 
 import java.util.Scanner;
 import static java.lang.System.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.Math;
 
 public class DogFoodRunner
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the weight of the dog each week, separating each number with a space: ");
-		DogFood doggy = new DogFood(scan.nextLine());
-		
+		Scanner scan = new Scanner(new File("DogFood.txt"));
+		   for(;scan.hasNext();)
+		   {
+			   String line = scan.nextLine();
+			   DogFood lc = new DogFood(line);
+			   System.out.println(lc.toString());
+		   }
 	}
 }
